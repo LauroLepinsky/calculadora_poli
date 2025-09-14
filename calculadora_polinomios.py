f5ae55ba4ca7586df16c01fb96da62d620ed03dc
Lauro Lepinsky <lauroisaaclepinsky@hotmail.com>
2025-09-14 19:31:00 -0300

    final

--- calculadora_polinomios.py
+++ calculadora_polinomios.py
@@ -1,4 +1,3 @@
-# %%
 import numpy as np
 import streamlit as st
 
@@ -7,13 +6,12 @@ st.title("Cálculo de raízes de polinômios: ")
 entrada_usuario = st.text_input(
     'Insira os coeficientes. Não precisa separá-los por vírgula. Em caso de números decimais, use " . ". Exemplo: 1 -5 6 '
 )
-lista = entrada_usuario.split()
+coeficientes = entrada_usuario.split()
 try:
-    coeficientes = [float(item) for item in lista]
-    print(coeficientes)
-except ValueError:
-    print(
-        'Insira apenas números, não precisa separá-los por vírgula. Em caso de números decimais, use "."'
+    coeficientes = [float(item) for item in coeficientes]
+except (ValueError, NameError):
+    st.write(
+        'ERRO: Insira APENAS números, não precisa separá-los por vírgula. Em caso de números decimais, use "."'
     )
 raizes_np = np.roots(coeficientes)
 raizes = [round(item, 2) for item in raizes_np]
