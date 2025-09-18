813fd1d52876013df0f63c4dc0ed651e32abfbfa
Lauro Lepinsky <lauroisaaclepinsky@hotmail.com>
2025-09-18 00:01:11 -0300

    correcao

--- calculadora_polinomios.py
+++ calculadora_polinomios.py
@@ -1,18 +1,25 @@
 import numpy as np
 import streamlit as st
 
-st.title("Cálculo de raízes de polinômios: ")
+st.title("Cálculo de raízes de polinômios")
 
 entrada_usuario = st.text_input(
-    'Insira os coeficientes. Não precisa separá-los por vírgula. Em caso de números decimais, use " . ". Exemplo: 1 -5 6 '
+    "Insira os coeficientes separados por espaço. Exemplo: 1 -5.5 6"
 )
-coeficientes = entrada_usuario.split()
-try:
-    coeficientes = [float(item) for item in coeficientes]
-except (ValueError, NameError):
-    st.write(
-        'ERRO: Insira APENAS números, não precisa separá-los por vírgula. Em caso de números decimais, use "."'
-    )
-raizes_np = np.roots(coeficientes)
-raizes = [round(item, 2) for item in raizes_np]
-st.write(f"Raízes: {raizes}")
+
+if entrada_usuario:
+
+    coeficientes_str = entrada_usuario.split()
+    try:
+        coeficientes_float = [float(item) for item in coeficientes_str]
+
+    except ValueError:
+        st.error(
+            'ERRO: Insira apenas números. Use "." para decimais e separe com espaço.'
+        )
+    else:
+        raizes_np = np.roots(coeficientes_float)
+        raizes_arredondadas = np.round(raizes_np, 5)
+        st.subheader("Raízes Encontradas:")
+        for i, raiz in enumerate(raizes_arredondadas):
+            st.success(f"**Raiz {i+1}:** `{np.round(raiz, 5)}`")
